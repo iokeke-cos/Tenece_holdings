a53e51a35d131cc452b948f3725bdd6dcfb7acbb
c.enujiokeCOS101 <chienujioke@gmail.com>
2025-08-22 09:34:57 +0100

    {Footer link edits)

--- Frontend/crystal-cleaners-frontend/src/components/Footer.jsx
+++ Frontend/crystal-cleaners-frontend/src/components/Footer.jsx
@@ -1,95 +1,108 @@
 /* I imported the icons i needed from the assets folder
 I also imported the css file for the Footer into this file to be able to use it*/
 
-import './Footer.css';
+import { FaChevronDown } from "react-icons/fa";
+import { Link, NavLink } from "react-router-dom";
+import { useState } from "react";
 
-import arrow from '../assets/arrowdown.png';
-import logo from '../assets/Logo.png';
-import fb from '../assets/fb.png';
-import twitter from '../assets/twitter.png';
-import linkedin from '../assets/linkedin.png';
+import "./Footer.css";
 
+import arrow from "../assets/arrowdown.png";
+import logo from "../assets/Logo.png";
+import fb from "../assets/fb.png";
+import twitter from "../assets/twitter.png";
+import linkedin from "../assets/linkedin.png";
 
 export default function Footer() {
+  const [isOpen, setIsOpen] = useState(false);
 
-    return(<>
-    {/* I added  */}
+  const toggleServices = () => {
+    setIsOpen(!isOpen);
+  };
 
-        <footer>
-            <div className="first-part">
-                <a href="#home">
-                <img src={logo} className='footer-logo'/>
-                </a>
-                <nav>
-                    <ul>
-                        <li>
-                            <a href="#home">Home</a>
-                        </li>
-                        <li>
-                            <a href="/about">About</a>
-                        </li>
-                        <li>
-                            <a href="/services">Services <span><img src={arrow} /></span></a>
-                        </li>
-                        <li>
-                            <a href="/faq">FAQ</a>
-                        </li>
-                        <li>
-                            <a href="/contact">Contact</a>
-                        </li>
-                        <li>
-                            <a href="/pricing">Pricing</a>
-                        </li>
-                    </ul>
-                    {/* these are the linkedin, X, and Facebook logos.
+  return (
+    <>
+      <footer>
+        <div className="first-part">
+          <Link to="/">
+            {" "}
+            <img src={logo} className="footer-logo" />
+          </Link>
+
+          <nav>
+            <ul>
+              <li>
+                <NavLink to="/">Home</NavLink>
+              </li>
+
+              <li>
+                <NavLink to="/about">About</NavLink>
+              </li>
+
+              <li>                
+                  <NavLink to="/services" className="nav-link">
+                    Services
+                  </NavLink>
+
+                                   
+                {/* I'm working on the drop down menu for the footer services tab */}
+
+                {/* {isOpen && (
+                  <ul className="dropdown-menu">
+                    <li>
+                      <NavLink>Service1</NavLink>
+                    </li>
+                    <li>
+                      <NavLink>Service2</NavLink>
+                    </li>
+                    <li>
+                      <NavLink>Service3</NavLink>
+                    </li>
+                  </ul>
+                )} */} 
+
+              </li>
+
+              <li>
+                <NavLink to="/faq">FAQ</NavLink>
+              </li>
+
+              <li>
+                <NavLink to="/contact">Contact</NavLink>
+              </li>
+
+              <li>
+                <NavLink to="pricing">Pricing</NavLink>
+              </li>
+            </ul>
+            {/* these are the linkedin, X, and Facebook logos.
                     I added an outer-logos div because I needed to adjust the position of the entire
                     logo container */}
 
-                    <div className='outer-logos'>
-
-                        <div className='logos'>
-                            <img src={twitter} alt="X"/>
-                            <img src={linkedin} alt="Linkedin"/>
-                            <img src={fb} alt="Facebook"/>                  
-                            
-
-                        </div>
-
-                    </div>
-
-                    </nav>
-
+            <div className="outer-logos">
+              <div className="logos">
+                <img src={twitter} alt="X" />
+                <img src={linkedin} alt="Linkedin" />
+                <img src={fb} alt="Facebook" />
+              </div>
             </div>
+          </nav>
+        </div>
 
-             <hr />
+        <hr />
 
-             {/* I separated the footer into the content before the line, and 
+        {/* I separated the footer into the content before the line, and 
              content after the line  */}
 
+        <div className="second-part">
+          <p className="copyright">© 2023 © 2023 Webtechsolution.in</p>
 
-            <div className='second-part'>
-
-                <p className='copyright'>© 2023 © 2023 Webtechsolution.in</p>
-
-                <p className='privacy'>
-                    <span className='privacy-policy'>Privacy Policy</span> | 
-                    <span className='cookies'> Cookies</span>
-
-                </p>
-                
-                
-            </div>              
-                
-
-            
-               
-                
-                
-                
-        </footer>
-    
-
-    
-    
-    </>);
+          <p className="privacy">
+            <span className="privacy-policy">Privacy Policy</span> |
+            <span className="cookies"> Cookies</span>
+          </p>
+        </div>
+      </footer>
+    </>
+  );
 }
